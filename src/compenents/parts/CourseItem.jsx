2126068b2925b462ec531f0981481ad3abb7d7ef
Humaidan Awad <humaidanawad@gmail.com>
2025-06-15 14:55:46 +0400

    new

--- src/compenents/parts/CourseItem.jsx
+++ src/compenents/parts/CourseItem.jsx
@@ -4,18 +4,18 @@ import { faThumbsUp, faUser } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function CourseItem({ language, link, item, continue_course, creator }) {
-    return (<Link to={link + item.id} key={"item-" + item.id} className="flex flex-col justify-between w-[23%] bg-white p-0 mx-2 my-3 hover:scale-102 font-bold h-[470px] rounded-2xl shadow group">
+    return (<Link to={link + item.id} key={"item-" + item.id} className="flex flex-col justify-between w-[23%] bg-white p-0 mx-2 my-3 hover:scale-102 font-bold h-[470px] rounded-xl shadow group">
         <div>
             <div className="relative p-0 mx-0 w-full flex items-center justify-center">
                 <img src="/play_btn.png" alt="" className="block mx-auto absolute z-10 w-12  group-hover:scale-95 transition-all" />
-                <img src={item.featured_image_url ? item.featured_image_url : '/data/sale-5.webp'} className="w-full h-[190px] object-cover rounded-t-2xl" />
+                <img src={item.featured_image_url ? item.featured_image_url : '/data/sale-5.webp'} className="w-full h-[190px] object-cover rounded-t-xl" />
             </div>
             <h3 className="text-xs mx-2 my-4 font-bold">{item.title}</h3>
-            <div className="h-[70px]">
+            <div className="h-[70px] text-color">
                 <p className="text-xs mx-2 my-2 font-light">{language && language['course_by']} {item.title}</p>
                 <p className="text-xs mx-2 font-light">{item.description}</p>
             </div>
-            <p className="text-xs font-light my-4 mx-2">
+            <p className="text-xs font-light my-4 mx-2 text-color">
                 <span className="">
                     <FontAwesomeIcon icon={faUser} className="text-xs font-light" /> 122.6 
                 </span>
