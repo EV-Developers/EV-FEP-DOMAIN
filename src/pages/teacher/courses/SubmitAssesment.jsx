2a6a7c856742b2fe36182a7571e84ee6e5033f33
Humaidan Awad <humaidanawad@gmail.com>
2025-07-29 15:55:35 +0400

    new

--- src/pages/teacher/courses/SubmitAssesment.jsx
+++ src/pages/teacher/courses/SubmitAssesment.jsx
@@ -295,55 +295,6 @@ export default function SubmitAssesment() {
         }
     }
 
-    const getCertificatePdf = () => {        
-        if(course){
-            const username = window.localStorage.getItem("VPHl3hMFGI8w9kq");
-            const courseTitle = course.category.name;
-            const date = new Date().toLocaleDateString('en-GB');
-
-            if (username != null && username != '') {
-                const cr_ref = String(Date.now());
-                // Replace file name spaces with dash
-                const file_name = courseTitle.replaceAll(' ', '-') + '-certificate.pdf';
-
-                // Add certificate URL as QR Code.
-                const qr = new QRCode(4, 'L');
-                qr.addData('https://fep.misk-donate.com/?c=' + cr_ref);
-                qr.make();
-                const qr_image = qr.toDataURL();
-
-                let logo = "/logo/Logo.png";
-                let bgcer = "/cerbg.png";
-
-                // creating certificate PDF
-                const doc = new jsPDF('landscape', 'pt', 'a4');
-                const centerX = doc.internal.pageSize.getWidth() / 2;
-                // get font name from 'Amiri-Regular-normal.js'
-                doc.setFont('Amiri-Regular');
-                doc.setTextColor('#212121');
-                doc.setFontSize(26);
-                doc.addImage(bgcer, 'png', 0, 0, 850, 600);
-                doc.addImage(logo, 'png', centerX - 70, 180, 142, 54);
-                doc.addImage(qr_image, 'png', centerX + 200, 450, 100, 100);
-                doc.setFontSize(32);
-                doc.text(username, centerX, 310, { align: 'center', dir: language['dir'] });
-                doc.setFontSize(18);
-
-                // if user UI language is arabic or english this will effect the certificate language.
-                if (language['dir'] == 'ltr') {
-                    doc.text("In " + course.category.name + " on " + date, centerX, 350, { align: 'center', dir: 'ltr' });
-                } else {
-                    doc.text("في برنامج " + course.category.name + " بتاريخ " + date, centerX, 350, { align: 'center', dir: 'rtl' });
-                }
-
-                //doc.save(file_name);
-                const pdfBlob = doc.output("blob", {filename: file_name});
-                
-                return pdfBlob;
-            }
-        }
-    }
-
     return (<ThemeContainer role="teachers">
         <form method="post" onSubmit={handleSubmitAssestment} encType="multipart/form-data" className="w-[75%] block mx-auto rounded-xl m-5 bg-white p-5">
             <div className="flex">
